5b6ba4f03abec58824753639b2e57b912ccce582
Galovach Andrey <a.galovach@yandex.ru>
2022-11-08 11:26:54 +0300

    task_05_OAuth Добавил GoogleRegister

--- auth/services/user/services/oauth.py
+++ auth/services/user/services/oauth.py
@@ -254,3 +254,50 @@ class YandexLogin(OAuthLogin):
             email=user['default_email'],
             social_id=user['id'],
         )
+
+
+class GoogleRegister(OAuthRegister):
+    def __init__(self) -> None:
+        super(GoogleRegister, self).__init__('google')
+        self.service = OAuth2Service(
+            name=self.consumer_name,
+            client_id=self.consumer_id,
+            client_secret=self.consumer_secret,
+            authorize_url=self.authorize_url,
+            access_token_url=self.access_token_url,
+            base_url=self.base_url,
+        )
+
+    def authorize(self):
+        return redirect(
+            self.service.get_authorize_url(
+                scope='email profile',
+                response_type='code',
+                redirect_uri=self.get_callback_url(),
+            ),
+        )
+
+    def callback(self):
+        def decode_json(payload):
+            return json.loads(payload.decode('utf-8'))
+
+        code = request.args.get('code', default=None)
+        if code is None:
+            raise exc.NoAccessError
+        oauth_session = self.service.get_auth_session(
+            method='POST',
+            data={
+                'grant_type': 'authorization_code',
+                'code': code,
+                'client_id': self.consumer_id,
+                'client_secret': self.consumer_secret,
+                'redirect_uri': self.get_callback_url(),
+            },
+            decoder=decode_json,
+        )
+        user = oauth_session.get('', params={'format': 'json'}).json()
+        return layer_models.OAuth(
+            username=user['name'],
+            email=user['email'],
+            social_id=user['id'],
+        )
