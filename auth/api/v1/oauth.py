38d4380bec8338430062801b2d59cc22477ffbfe
Galovach Andrey <a.galovach@yandex.ru>
2022-11-08 10:00:54 +0300

    task_14_OAuth Добавил ручки /login/<provider> и /login-callback/<provider>

--- auth/api/v1/oauth.py
+++ auth/api/v1/oauth.py
@@ -3,7 +3,7 @@ from http import HTTPStatus
 from core.config import settings
 from flask import Blueprint, jsonify, request
 from services.user.payload_models import OAuthPayload
-from services.user.services.oauth import OAuthRegister
+from services.user.services.oauth import OAuthLogin, OAuthRegister
 from utils.exceptions import EmailAlreadyExist, NoAccessError, UniqueConstraintError
 
 oauth_blueprint = Blueprint('oauth', __name__, url_prefix='/api/v1/oauth')
@@ -44,7 +44,7 @@ def oauth_register_callback(provider):
      summary: Регистрация нового пользователя через OAuth.
      responses:
        '200':
-         description: Login successful
+         description: Register successful
        '403':
          description: Permission denied
        '409':
@@ -66,3 +66,49 @@ def oauth_register_callback(provider):
         return jsonify(message='Social account is already linked'), HTTPStatus.CONFLICT
 
     return jsonify(message='New user was registered'), HTTPStatus.OK
+
+
+@oauth_blueprint.route('/login/<provider>')
+def oauth_login(provider):
+    """
+    Взод пользователя через OAuth.
+    ---
+    post:
+     summary: Взод пользователя через OAuth
+     parameters:
+      - name: provider
+        in: path
+        type: string
+        required: true
+     responses:
+       '200':
+         description: Login successful
+       '404':
+         description: Unsupportable provider
+     tags:
+       - OAuth
+    """
+    if not settings.oauth.providers.check_value(provider):
+        return jsonify(message='Unsupportable provider'), HTTPStatus.NOT_FOUND
+    oauth = OAuthLogin.get_provider(provider)
+    return oauth.authorize()
+
+
+@oauth_blueprint.route('/login-callback/<provider>')
+def oauth_login_callback(provider):
+    """
+    Взод пользователя через OAuth.
+    ---
+    get:
+     summary: Взод пользователя через OAuth.
+     responses:
+       '200':
+         description: Login successful
+       '403':
+         description: Permission denied
+       '409':
+         description: Email is already in use or social account is already linked to another user
+     tags:
+       - OAuth
+    """
+    ...
