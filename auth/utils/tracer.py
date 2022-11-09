669e9a2cff795620a443216a59ae27a641e3b5b3
Galovach Andrey <a.galovach@yandex.ru>
2022-11-09 07:33:37 +0300

    task_17_Trace Добавил JaegerSettings, Обновил документацию

--- auth/utils/tracer.py
+++ auth/utils/tracer.py
@@ -1,17 +1,26 @@
+from core.config import settings
+from flask import Flask
 from opentelemetry import trace
 from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
 
 
-def configure_tracer(app) -> None:
-    trace.set_tracer_provider(TracerProvider())
+def configure_tracer(app: Flask) -> None:
+    resource = Resource(
+        {
+            'service.name': settings.jaeger.SERVICE_NAME,
+        },
+    )
+    provider = TracerProvider(resource=resource)
+    trace.set_tracer_provider(provider)
     trace.get_tracer_provider().add_span_processor(
         BatchSpanProcessor(
             JaegerExporter(
-                agent_host_name='localhost',
-                agent_port=6831,
+                agent_host_name=settings.jaeger.AGENT_HOST,
+                agent_port=settings.jaeger.AGENT_PORT,
             ),
         ),
     )
